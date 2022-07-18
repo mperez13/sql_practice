da40cdbb53c2b1e9ad2eb1df5fb593bd4e7a3a75
mperez <19216545+mperez13@users.noreply.github.com>
2022-07-17 21:59:15 -0700

    final shippers #1

--- 01-Return_Shippers.sql
+++ 01-Return_Shippers.sql
@@ -2,17 +2,17 @@
 DROP TABLE Shippers;
 
 -- Create a table name Shippers
-CREATE TABLE Shippers {
+CREATE TABLE Shippers (
   ShipperID int NOT NULL,
   CompanyName varchar(255) NOT NULL,
-  Phone int
+  Phone varchar(20)
   CONSTRAINT PK_Shipper PRIMARY KEY (ShipperID)
-}
+)
 -- Insert all all the entries for the table Shippers.
 INSERT INTO Shippers (ShipperID, CompanyName, Phone)
-VALUES (1,Speedy Express, 5035559831),
-  (2,United Package,5035553199),
-  (3,Federal Shipping,5035559931);
+VALUES (1,'Speedy Express', '(503) 555-9831'),
+  (2,'United Package','(503) 555-3199'),
+  (3,'Federal Shipping','(503) 555-9931');
 
 -- Return all fields from all the shippers.
 SELECT * FROM Shippers;
