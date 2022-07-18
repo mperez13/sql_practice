afe6e787d6c003ef770573afdec37ec7ced2f818
mperez <19216545+mperez13@users.noreply.github.com>
2022-07-17 23:13:34 -0700

    fixed employee table

--- 03-Sales.sql
+++ 03-Sales.sql
@@ -1,4 +1,4 @@
 -- Return the FirstName, LastName and HireDate of all employees with the Title of Sales Representative.
 SELECT FirstName, LastName, HireDate
-FROM SalesRepresentative
-WHERE Title='SalesRepresentative'; 
+FROM Employees
+WHERE Title='Sales Representative';
