9c2b86fc15449ffd58057579a46e576f4abf4d27
mperez <19216545+mperez13@users.noreply.github.com>
2022-07-17 23:09:53 -0700

    restructure

---
+++ 03-Sales.sql
@@ -0,0 +1,4 @@
+-- Return the FirstName, LastName and HireDate of all employees with the Title of Sales Representative.
+SELECT FirstName, LastName, HireDate
+FROM SalesRepresentative
+WHERE Title='SalesRepresentative'; 
