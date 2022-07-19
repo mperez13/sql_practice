31bcdd2f01541fc653bbe7b6e63969cc9f966be7
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-07-19 13:01:06 -0700

    #11 completed

--- Introductory-Problems/11-Date.sql
+++ Introductory-Problems/11-Date.sql
@@ -1,4 +1,5 @@
 -- For all the employees in the Employes table, show the FirstName, LastName, Title, and BirthDate. Order the results by BirthDate, so we have the oldest employees first. On the BirthDate field, show only the date portion.
-SELECT FirstName, LastName, Title, BirthDate 
+SELECT FirstName, LastName, Title, CONVERT(varchar(10), BirthDate, 110) AS DateOnlyBirthDate 
 FROM Employees
 ORDER BY BirthDate desc;
+
