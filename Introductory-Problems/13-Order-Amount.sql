ff4fe1607ba0d7ba1c7e82370f0e9e406b67683b
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-07-19 13:26:24 -0700

    #13 completed

--- Introductory-Problems/13-Order-Amount.sql
+++ Introductory-Problems/13-Order-Amount.sql
@@ -1,4 +1,6 @@
--- In the OrderDetails table, create a new field, TotalPrice, that multiplies these two together. Ignore the Discount field.
+-- In the OrderDetails table, create a new field, TotalPrice, that multiplies UnitPrice and Quantity. Ignore the Discount field.
+ALTER TABLE OrderDetails
+ADD TotalPrice AS (UnitPrice*Quantity) PERSISTED;
 
 -- Show the OrderID, ProductID, UnitPrice and Quantity. Order by OrderID and ProductID
 SELECT OrderID, ProductID, UnitPrice, Quantity
