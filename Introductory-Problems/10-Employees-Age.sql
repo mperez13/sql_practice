06f9cfcc99e5a37c6fc9b913fd94d2485b8eb4f8
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-07-19 11:53:00 -0700

    Update 10-Employees-Age.sql

--- Introductory-Problems/10-Employees-Age.sql
+++ Introductory-Problems/10-Employees-Age.sql
@@ -1,4 +1,4 @@
 -- For all the employees in the Employes table, show the FirstName, LastName, Title, and BirthDate. Order the results by BirthDate, so we have the oldest employees first.
 SELECT FirstName, LastName, Title, BirthDate 
 FROM Employees
-ORDER BY BirthDate asc;
+ORDER BY BirthDate desc;
