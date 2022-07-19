06bd702fc175caf2d1b0010b8ea2faf8a59505d6
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-07-19 13:11:17 -0700

    Create 12-Employee-Name.sql

---
+++ Introductory-Problems/12-Employee-Name.sql
@@ -0,0 +1,3 @@
+-- Show the FirstName and LastName columns from the Employees table, and then create a new column called FullName, showing FirstName and LastName joined together in one column, with a space inbetween
+SELECT FirstName + ' ' + LastName AS FullName
+FROM Employees;
