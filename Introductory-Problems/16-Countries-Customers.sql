1cd72c2f4f1e320d28773185992df50e1080a0df
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-07-20 10:30:25 -0700

    fixed order by

--- Introductory-Problems/16-Countries-Customers.sql
+++ Introductory-Problems/16-Countries-Customers.sql
@@ -1,2 +1,2 @@
 -- Show a list of countries where the Northwind company has customers.
-SELECT DISTINCT Country FROM Customers ORDERBY Country asc;
+SELECT DISTINCT Country FROM Customers ORDER BY Country asc;
