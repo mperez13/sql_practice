3f25cd5f9cd8dbb464f0c8e91a1753759ebdee1c
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-07-21 14:29:47 -0700

    Create 22.sql

---
+++ Intermediate-Problems/22.sql
@@ -0,0 +1,5 @@
+-- Use fields UnitsInStock and ReoroderLevel, where UnitsInStock is less than or equal to the ReorderLevel. Sort by ProductID
+SELECT ProductID, ProductName, UnitsInStock, ReorderLevel
+FROM Products
+WHERE UnitsInStock <= ReorderLevel
+ORDER BY ProductID;
