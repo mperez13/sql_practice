186097b957794d1e601473167656c200f31930cb
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-07-21 14:22:48 -0700

    #21 completed

---
+++ Intermediate-Problems/21-Customer-Country-City.sql
@@ -0,0 +1,5 @@
+-- In the customers table, show the total number of customers per Country and City.
+SELECT Country, City, Count(CustomerID) AS TotalCustomer
+FROM Customers
+GROUP BY Country, City
+ORDER BY TotalCustomer desc;
