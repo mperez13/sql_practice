9447b2645aee9faf7d014692c9efa3c57c6a44d2
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-07-21 14:45:51 -0700

    Create 24.sql

---
+++ Intermediate-Problems/24.sql
@@ -0,0 +1,4 @@
+-- Show all customers, sorted by region, alphabetically. Customers with no region should be at the end of the list. Companies should be sorted by CustomerID.
+SELECT * 
+FROM Customers
+ORDER BY Region desc;
