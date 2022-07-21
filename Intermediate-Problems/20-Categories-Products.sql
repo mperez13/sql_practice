c4be7d091248c21d9a623780d5aeded45618199e
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-07-21 13:48:49 -0700

    #20 completed

--- Intermediate-Problems/20-Categories-Products.sql
+++ Intermediate-Problems/20-Categories-Products.sql
@@ -1,4 +1,5 @@
 -- Show the total number of products in each category. Sort the results by the total number of products, in descending order.
 SELECT c.CategoryName, Count(p.CategoryID) AS TotalNumberOfProducts
-FROM Products p JOIN Categories c
-ON p.CategoryID = c.CategoryID;
+FROM Categories c INNER JOIN Products p
+ON c.CategoryID = p.CategoryID
+GROUP BY c.CategoryName;
