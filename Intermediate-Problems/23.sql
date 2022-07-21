e3583111e980b3d1812fe6e35a2700dce1c992af
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-07-21 14:41:06 -0700

    #23 completed

--- Intermediate-Problems/23.sql
+++ Intermediate-Problems/23.sql
@@ -1,5 +1,8 @@
-*/ Now we need to incorporate these fields—UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued—into our calculation
--- We’ll define “products that need reordering” with the following:
-UnitsInStock plus UnitsOnOrder are less than
-or equal to ReorderLevel
-The Discontinued flag is false (0). */
+/* Now we need to incorporate these fields— UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued —into our calculation
+We’ll define “products that need reordering” with the following:
+- UnitsInStock plus UnitsOnOrder are less than or equal to ReorderLevel
+- The Discontinued flag is false (0). */
+SELECT ProductID, ProductName, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued
+FROM Products
+WHERE UnitsInStock + UnitsOnOrder <= ReorderLevel AND Discontinued = 0
+ORDER BY ProductID;
