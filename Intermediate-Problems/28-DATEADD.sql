7b8d5a8fd42c6cf8cd21ae144c8df2a35a0c063a
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-07-22 11:48:38 -0700

    Update 28-DATEADD.sql

--- Intermediate-Problems/28-DATEADD.sql
+++ Intermediate-Problems/28-DATEADD.sql
@@ -1,6 +1,6 @@
 -- Show the three ship countries with the highest average freight charges of the last 12 months of order data
 SELECT Top(3) ShipCountry, AVG(Freight) As AverageFreight
 FROM Orders
-WHERE OrderDate > DATEADD(year,-1,OrderDate)
+WHERE OrderDate >= DATEADD(year,-1,OrderDate)
 GROUP BY ShipCountry
 ORDER BY AverageFreight desc;
