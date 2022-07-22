5a63a16662d7c5dd06227a17b899608dc3d45dd7
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-07-22 09:08:13 -0700

    renamed file- completed

--- Intermediate-Problems/24-Case-when.sql
+++ Intermediate-Problems/24-Case-when.sql
@@ -1,4 +1,5 @@
 -- Show all customers, sorted by region, alphabetically. Customers with no region should be at the end of the list. Companies should be sorted by CustomerID.
-SELECT * 
+SELECT CustomerID, CompanyName, Region, 
+CASE WHEN Region is NULL THEN 1 ELSE 0 END AS isNULL
 FROM Customers
-ORDER BY CustomerID asc, Region desc;
+ORDER BY isNUll, Region;
