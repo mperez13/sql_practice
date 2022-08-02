3a7146cbb76a18bec89778652690c917d840220a
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-08-02 15:01:54 -0700

    #30 completed

--- Intermediate-Problems/30-Left-Join.sql
+++ Intermediate-Problems/30-Left-Join.sql
@@ -1,1 +1,5 @@
 -- Show Customers that have never placed an order.
+SELECT DISTINCT(c.CustomerID) AS Customers_CustomerID, o.CustomerID AS Orders_CustomerID
+FROM Customers c FULL OUTER JOIN Orders o
+ON c.CustomerID = o.CustomerID
+WHERE o.CustomerID IS NULL;
