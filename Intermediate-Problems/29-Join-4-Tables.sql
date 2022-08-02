d4ab32da54477dc15f9a25fb488f9c7db8614bdb
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-08-02 14:51:03 -0700

    #29 completed

--- Intermediate-Problems/29-Join-4-Tables.sql
+++ Intermediate-Problems/29-Join-4-Tables.sql
@@ -1,6 +1,7 @@
 -- Show the EmployeeID, LastName, OrderID, ProductName, Quantity. Sort by OrderID and Product ID.
-SELECT e.EmployeeID, e.LastName, o.OrderID, ProductName, Quantity 
-FROM Orders o JOIN Employees e
-ON o.employeeID = e.employeeID
-JOIN Product p
-ON ;
+SELECT o.EmployeeID, e.LastName, o.OrderID, p.ProductName, od.Quantity 
+FROM Orders o 
+INNER JOIN Employees e ON o.employeeID = e.employeeID
+INNER JOIN OrderDetails od ON o.orderID = od.orderID
+INNER JOIN Products p ON p.productID = od.productID
+ORDER BY orderID, p.productID;
