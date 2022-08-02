3f1d0d0748cf82dde7c090132b71997af9f1b9cd
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-08-01 18:43:56 -0700

    Update 29-Join-4-Tables.sql

--- Intermediate-Problems/29-Join-4-Tables.sql
+++ Intermediate-Problems/29-Join-4-Tables.sql
@@ -1,1 +1,6 @@
 -- Show the EmployeeID, LastName, OrderID, ProductName, Quantity. Sort by OrderID and Product ID.
+SELECT e.EmployeeID, e.LastName, o.OrderID, ProductName, Quantity 
+FROM Orders o JOIN Employees e
+ON o.employeeID = e.employeeID
+JOIN Product p
+ON ;
