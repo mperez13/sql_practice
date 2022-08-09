e72a7a57bc46d8d7fbfe1251e9f0d7327041b98b
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-08-09 12:58:35 -0700

    #42 completed

--- advanced-problems/42.sql
+++ advanced-problems/42.sql
@@ -1,1 +1,8 @@
 - Show which salespeople have the most orders arriving late
+SELECT e.EmployeeID, e.LastName, Count(e.employeeID) AS TotalLateOrders
+FROM Orders o
+JOIN Employees e 
+ON o.EmployeeID = e.EmployeeID
+WHERE ShippedDate >= RequiredDate
+GROUP BY e.EmployeeID, e.LastName
+ORDER BY TotalLateOrders desc;
