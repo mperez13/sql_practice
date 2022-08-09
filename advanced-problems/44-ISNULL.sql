0fc23bb27d6f1a7c4bc3d8d52a1abdea3cca2a58
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-08-09 14:05:17 -0700

    #44 completed

---
+++ advanced-problems/44-ISNULL.sql
@@ -0,0 +1,19 @@
+-- Fix the NULL in #43. Show 0 if answer is NULL
+WITH AllOrders_CTE AS
+(
+SELECT employeeID, Count(employeeID) AS AllOrders
+FROM Orders 
+GROUP BY EmployeeID
+),
+TotalLateOrders_CTE AS
+(
+SELECT employeeID, Count(employeeID) AS TotalLateOrders
+FROM Orders 
+WHERE ShippedDate >= RequiredDate
+GROUP BY EmployeeID
+)
+SELECT e.EmployeeID, e.LastName, ISNULL(a.AllOrders,0), ISNULL(l.TotalLateOrders,0)
+FROM Employees e
+JOIN AllOrders_CTE a ON a.EmployeeID = e.EmployeeID
+LEFT JOIN TotalLateOrders_CTE l ON l.EmployeeID = e.EmployeeID
+ORDER BY e.EmployeeID asc;
