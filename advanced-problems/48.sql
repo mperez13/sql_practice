25ba1906eaf7a6f3b41d406718c3740139bb0a8a
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-08-09 15:53:34 -0700

    Create 48.sql

---
+++ advanced-problems/48.sql
@@ -0,0 +1,9 @@
+/* Categorize cusomters into groups based on how much they ordered in 1998. Depending on which group the customer is, the customer with different sales materials will be targeted. 
+The customer grouping categories:
+0 to 1,000
+1,000 to 5,000
+5,000 to 10,000
+over 10,000
+
+
+*/
