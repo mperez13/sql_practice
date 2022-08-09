27d967cb904b09ba099da0fae44b2b4ef2abfa8d
Maria Perez <19216545+mperez13@users.noreply.github.com>
2022-08-09 12:33:31 -0700

    Create 40-.sql

---
+++ advanced-problems/40-.sql
@@ -0,0 +1,8 @@
+-- Another way of completing #39 is using a derived table as shown as the SQL below. Fix the error in the SQL.
+/*
+SELECT OrderDetails.OrderID, ProductID, UnitPrice, Quantity, Discount
+FROM OrderDetails
+JOIN (SELECT OrderID FROM OrderDetails WHERE Quantity >=60 GROUP BY OrderID, Quantity Having Count(*) > 1)
+PotentialProblemOrders ON PotentialProblemOrders.OrderID = OrderDetails.OrderID
+ORDER BY OrderID, ProductID
+*/
