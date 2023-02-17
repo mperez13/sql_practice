c55011ed73ac7604575f71a3b5d9bdb1fa9d646c
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-02-17 13:57:37 -0800

    Update 50.sql

--- Vasilik_Practice_Problems/advanced-problems/50.sql
+++ Vasilik_Practice_Problems/advanced-problems/50.sql
@@ -1,4 +1,14 @@
--- Based on the query from #48,49, show all the defined CustomerGroups, and the percentage in each. Sort by the total in each group, in descending order.
+/* 
+Based on the query from #48,49, show all the defined CustomerGroups, and the percentage in each. Sort by the total in each group, in descending order.
+	Expected Results:
+	CustomerGroup | TotalInGroup | PercentageInGroup
+	Medium	      | 35           | 0.432098765432
+	Low           | 20           | 0.246913580246
+	High          | 13           | 0.160493827160
+	Very High     | 13           | 0.160493827160
+Hint #1: As a starting point, you can use the answer from the problem "Customer grouping - fix null".
+Hint #2: We no longer need to show the CustomerID and CompanyName in the final output. However, we need to count how many customers are in each CustomerGrouping. You can create another CTE level in order to get the counts in each CustomerGrouping for the final output.
+*/
 WITH CustomerGroup_CTE AS (
 SELECT o.CustomerID, c.CompanyName, SUM(UnitPrice*Quantity) AS TotalOrderAmount,
 CASE
