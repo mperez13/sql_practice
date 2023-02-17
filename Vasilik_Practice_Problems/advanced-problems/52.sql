c4aa20beed910e581bbce70c252dcf6ec5181600
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-02-17 14:12:37 -0800

    Update 52.sql

--- Vasilik_Practice_Problems/advanced-problems/52.sql
+++ Vasilik_Practice_Problems/advanced-problems/52.sql
@@ -1,5 +1,5 @@
 /* Countries with suppliers or customers
-Some Northwind employees are planning a business trip, and would like to visit as many suppliers and customers as possible. For their planning, they’d like to see a list of all countries where suppliers and/or customers are based.
+Some Northwind employees are planning a business trip, and would like to visit as many suppliers and customers as possible. For their planning, they’d like to see a list of all countries where suppliers and/or customers are based. 
     Expected Results
     Country
     ---------------
