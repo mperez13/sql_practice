0db39776d4b93147a2c76b218bebebcfdd36ca82
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-02-17 14:12:06 -0800

    Create 52.sql

---
+++ Vasilik_Practice_Problems/advanced-problems/52.sql
@@ -0,0 +1,33 @@
+/* Countries with suppliers or customers
+Some Northwind employees are planning a business trip, and would like to visit as many suppliers and customers as possible. For their planning, they’d like to see a list of all countries where suppliers and/or customers are based.
+    Expected Results
+    Country
+    ---------------
+    Argentina
+    Australia
+    Austria
+    Belgium
+    Brazil
+    Canada
+    Denmark
+    Finland
+    France
+    Germany
+    Ireland
+    Italy
+    Japan
+    Mexico
+    Netherlands
+    Norway
+    Poland
+    Portugal
+    Singapore
+    Spain
+    Sweden
+    Switzerland
+    UK
+    USA
+    Venezuela
+    (25 row(s) affected)
+Hint: Use the Union statement for this. It's a good way of putting together a simple resultset from multiple SQL statements.
+*/
