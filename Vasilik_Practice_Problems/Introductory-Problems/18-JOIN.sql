313ae1f46feebcab4450c2cc1a971c74804b0b46
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-20 14:20:21 -0700

    #18 completed

---
+++ Vasilik_Practice_Problems/Introductory-Problems/18-JOIN.sql
@@ -0,0 +1,6 @@
+-- #18 For each product show the associated Supplier. Show the ProductID, ProductName and the CompanyName of the Supplier. Sort by ProductID
+SELECT p.ProductID, p.ProductName, s.CompanyName
+FROM Products p JOIN Suppliers s
+ON p.SupplierID = s.SupplierID
+ORDER BY ProductID;
+
