f50d7871f2a48393cc8a0feb843e955ff39c6c97
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-20 14:15:55 -0700

    #17 completed

--- Vasilik_Practice_Problems/Introductory-Problems/17-Contact-Customers.sql
+++
@@ -1,4 +0,0 @@
--- Show a list of all the different values in the Customers table for ContactTitles. Include the count for each ContactTitle.
-SELECT ContactTitle, Count(*) AS TotalContactTitle
-FROM Customers
-GROUP BY ContactTitle;
