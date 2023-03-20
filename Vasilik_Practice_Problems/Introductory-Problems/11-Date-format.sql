6136b4e441cc466eb9b99cc5bc77062d1a9a376e
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-20 14:01:47 -0700

    #11 completed

---
+++ Vasilik_Practice_Problems/Introductory-Problems/11-Date-format.sql
@@ -0,0 +1,5 @@
+-- #11 For all the employees in the Employes table, show the FirstName, LastName, Title, and BirthDate. Order the results by BirthDate, so we have the oldest employees first. On the BirthDate field, show only the date portion; exclude the tiem portion.
+SELECT FirstName, LastName, Title, CONVERT(varchar(10), BirthDate, 110) AS DateOnlyBirthDate 
+FROM Employees
+ORDER BY BirthDate desc;
+
--- Vasilik_Practice_Problems/Introductory-Problems/11-Date.sql
+++
@@ -1,5 +0,0 @@
--- For all the employees in the Employes table, show the FirstName, LastName, Title, and BirthDate. Order the results by BirthDate, so we have the oldest employees first. On the BirthDate field, show only the date portion.
-SELECT FirstName, LastName, Title, CONVERT(varchar(10), BirthDate, 110) AS DateOnlyBirthDate 
-FROM Employees
-ORDER BY BirthDate desc;
-
