9ead0570025429e5fb9c17952ccbe7a3e6860f47
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-20 14:21:19 -0700

    #19 completed

--- Vasilik_Practice_Problems/Introductory-Problems/19-JOIN-WHERE.sql
+++ Vasilik_Practice_Problems/Introductory-Problems/19-JOIN-WHERE.sql
@@ -1,4 +1,4 @@
--- Show OrderID, OrderDate (date only), and CompanyName of the Shipper. Sort by OrderID. Show only orders with OrderID less than 10300.
+-- #19 Show OrderID, OrderDate (date only), and CompanyName of the Shipper. Sort by OrderID. Show only orders with OrderID less than 10300.
 SELECT Orders.OrderID, convert(varchar(20),Orders.OrderDate,110) AS OrderDate, Shippers.CompanyName
 FROM Shippers JOIN Orders 
 ON Shippers.ShipperID = Orders.ShipVia
