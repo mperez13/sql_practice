35b693da459ae62c2e07a27c0e09af6e20259370
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-20 12:45:44 -0700

    #7 completed

--- Vasilik_Practice_Problems/Introductory-Problems/07-products-queso.sql
+++
@@ -1,4 +0,0 @@
--- In the products table, we'd like to see the ProductID and ProductName for those products where the ProductName includes the string "queso".
-SELECT ProductID, ProductName
-FROM Products
-WHERE ProductName LIKE '%queso%';
