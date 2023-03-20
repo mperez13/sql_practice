29577b17e3b745e7a8ea784c925ce8f155f450c6
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-20 13:58:37 -0700

    #9 completed

--- Vasilik_Practice_Problems/Introductory-Problems/09-WHERE-IN.sql
+++ Vasilik_Practice_Problems/Introductory-Problems/09-WHERE-IN.sql
@@ -1,4 +1,4 @@
--- Show all the orders from any Latin American country.
+-- #9 Show all the orders from any Latin American country.
 SELECT OrderID, CustomerID, ShipCountry 
 FROM ORDERS 
 WHERE ShipCountry IN ('Brazil', 'Mexico', 'Venezuela');
