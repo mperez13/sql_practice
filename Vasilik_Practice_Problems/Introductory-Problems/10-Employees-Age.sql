b20131e29bdbe89f0fd60c5e8c7d111137372c10
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-20 14:00:18 -0700

    #10 completed

--- Vasilik_Practice_Problems/Introductory-Problems/10-Employees-Age.sql
+++
@@ -1,4 +0,0 @@
--- For all the employees in the Employes table, show the FirstName, LastName, Title, and BirthDate. Order the results by BirthDate, so we have the oldest employees first.
-SELECT FirstName, LastName, Title, BirthDate 
-FROM Employees
-ORDER BY BirthDate desc;
---
+++ Vasilik_Practice_Problems/Introductory-Problems/10-ORDER-BY.sql
@@ -0,0 +1,4 @@
+-- #10 For all the employees in the Employes table, show the FirstName, LastName, Title, and BirthDate. Order the results by BirthDate, so we have the oldest employees first.
+SELECT FirstName, LastName, Title, BirthDate 
+FROM Employees
+ORDER BY BirthDate desc;
