c3ba80a0d99688efdd5ec960d748e596cad42e36
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-21 14:55:30 -0700

    #48 completed

--- Vasilik_Practice_Problems/advanced-problems/48-CASE.sql
+++ Vasilik_Practice_Problems/advanced-problems/48-CASE.sql
@@ -1,4 +1,4 @@
-/* Categorize cusomters into groups based on how much they ordered in 1998. Depending on which group the customer is, the customer with different sales materials will be targeted.
+/* #48 Categorize cusomters into groups based on how much they ordered in 1998. Depending on which group the customer is, the customer with different sales materials will be targeted.
 The customer grouping categories:
 LOW - 0 to 1,000
 MEDIUM - 1,000 to 5,000
