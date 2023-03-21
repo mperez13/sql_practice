29b90c9e7b37360230f189a28e030c5c798c7ddb
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-21 14:18:24 -0700

    #37 completed

--- Vasilik_Practice_Problems/advanced-problems/37-TOP-PERCENT.sql
+++ Vasilik_Practice_Problems/advanced-problems/37-TOP-PERCENT.sql
@@ -1,4 +1,4 @@
--- Show a random assortment of orders. Show a random set of 2% of all orders.
+-- #37 Show a random assortment of orders. Show a random set of 2% of all orders.
 SELECT TOP 2 PERCENT OrderID, NewID() AS RandomValue
 FROM OrderDetails
 ORDER BY RandomValue;
