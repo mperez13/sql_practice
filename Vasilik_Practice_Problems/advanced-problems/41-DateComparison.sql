7aa888d45b0217c6f7508bf1fb833f944e35ccd6
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-21 14:48:45 -0700

    #41 completed

--- Vasilik_Practice_Problems/advanced-problems/41-DateComparison.sql
+++ Vasilik_Practice_Problems/advanced-problems/41-DateComparison.sql
@@ -1,4 +1,4 @@
--- Show orders that are arriving late.
+-- #41 Show orders that are arriving late.
 SELECT OrderID, OrderDate, RequiredDate, ShippedDate
 FROM Orders
-WHERE ShippedDate >= RequiredDate;
+WHERE ShippedDate > RequiredDate;
