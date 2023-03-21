53a99afa2f83eb47610c0bfe585fe3c920e4a112
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-21 14:16:01 -0700

    #36 completed

--- Vasilik_Practice_Problems/advanced-problems/36-CountGroup.sql
+++
@@ -1,5 +0,0 @@
--- Show orders that have lots of individual line items. Show the 10 orders with the most items, in order of total line items.
-SELECT TOP 10 OrderID, Count(OrderID) AS TotalOrderDetails
-FROM OrderDetails
-GROUP BY OrderID
-ORDER BY TotalOrderDetails desc;
