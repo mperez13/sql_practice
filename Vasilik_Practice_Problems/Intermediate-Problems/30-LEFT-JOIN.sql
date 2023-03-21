5b6f4e1c46448311152601c1760f986f8d6e06af
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-21 10:55:50 -0700

    #30 completed

--- Vasilik_Practice_Problems/Intermediate-Problems/30-LEFT-JOIN.sql
+++ Vasilik_Practice_Problems/Intermediate-Problems/30-LEFT-JOIN.sql
@@ -1,5 +1,5 @@
--- Show Customers that have never placed an order.
+-- #30 Show Customers that have never placed an order.
 SELECT DISTINCT(c.CustomerID) AS Customers_CustomerID, o.CustomerID AS Orders_CustomerID
-FROM Customers c FULL OUTER JOIN Orders o
+FROM Customers c LEFT JOIN Orders o
 ON c.CustomerID = o.CustomerID
 WHERE o.CustomerID IS NULL;
