68800c8c3207601bc911616621c877504084a20f
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-21 08:57:26 -0700

    #20 completed

--- Vasilik_Practice_Problems/Intermediate-Problems/20-Categories-Products.sql
+++
@@ -1,5 +0,0 @@
--- Show the total number of products in each category. Sort the results by the total number of products, in descending order.
-SELECT c.CategoryName, Count(p.CategoryID) AS TotalNumberOfProducts
-FROM Categories c INNER JOIN Products p
-ON c.CategoryID = p.CategoryID
-GROUP BY c.CategoryName;
---
+++ Vasilik_Practice_Problems/Intermediate-Problems/20-JOIN-GROUPBY.sql
@@ -0,0 +1,6 @@
+-- #20 Show the total number of products in each category. Sort the results by the total number of products, in descending order.
+SELECT c.CategoryName, Count(p.CategoryID) AS TotalNumberOfProducts
+FROM Categories c INNER JOIN Products p
+ON c.CategoryID = p.CategoryID
+GROUP BY c.CategoryName
+ORDER BY TotalNumberOfProducts desc;
