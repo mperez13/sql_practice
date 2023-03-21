441d329e1ff7c6575b30eaa872210d3790410d86
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-21 14:24:05 -0700

    #38 completed

--- Vasilik_Practice_Problems/advanced-problems/38-WHERE-HAVING.sql
+++ Vasilik_Practice_Problems/advanced-problems/38-WHERE-HAVING.sql
@@ -1,5 +1,5 @@
--- Show all orderIDs with line items of 60 or more. Order by OrderID
-SELECT OrderID, Quantity
+-- #38 Show all orderIDs with line items of 60 or more. Order by OrderID
+SELECT OrderID
 FROM OrderDetails
 WHERE Quantity >= 60
 GROUP BY OrderID, Quantity
