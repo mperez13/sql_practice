193f4bf12d6fe14ac158e550bb204e3a708c1e18
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-21 10:58:13 -0700

    #32 completed

--- Vasilik_Practice_Problems/advanced-problems/32-INNER-JOIN.sql
+++ Vasilik_Practice_Problems/advanced-problems/32-INNER-JOIN.sql
@@ -1,4 +1,4 @@
--- Display high-value customers. High-value customers are those who've made at least 1 order with a total value (not including the discount) equal to $10,000 or more. Only display orders made in year 1998.
+-- #32 Display high-value customers. High-value customers are those who've made at least 1 order with a total value (not including the discount) equal to $10,000 or more. Only display orders made in year 1998.
 SELECT c.CustomerID, c.CompanyName, o.OrderID, SUM(oD.UnitPrice*oD.Quantity) AS TotalOrderAmount
 FROM Customers c
 INNER JOIN Orders o ON o.CustomerID = c.CustomerID
