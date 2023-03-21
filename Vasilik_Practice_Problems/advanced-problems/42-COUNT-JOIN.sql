aeb82d59e1f1d5b6e19ed3a6de30c65e32324774
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-21 14:51:04 -0700

    #42 completed

--- Vasilik_Practice_Problems/advanced-problems/42-COUNT-JOIN.sql
+++ Vasilik_Practice_Problems/advanced-problems/42-COUNT-JOIN.sql
@@ -1,4 +1,4 @@
-- Show which salespeople have the most orders arriving late
+- #42 Show which salespeople have the most orders arriving late
 SELECT e.EmployeeID, e.LastName, Count(e.employeeID) AS TotalLateOrders
 FROM Orders o
 JOIN Employees e 
