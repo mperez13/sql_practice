3ac99a6c787a781ac5a3b5be68c702c9cd698ffd
Maria Perez <19216545+mperez13@users.noreply.github.com>
2023-03-21 10:30:51 -0700

    #25 completed

--- Vasilik_Practice_Problems/Intermediate-Problems/25-TOP-AVERAGE.sql
+++ Vasilik_Practice_Problems/Intermediate-Problems/25-TOP-AVERAGE.sql
@@ -1,6 +1,5 @@
--- Return the three ship countries with the highest freight overall, in descending order by average freight.
+-- #25 Return the three ship countries with the highest freight overall, in descending order by average freight.
 SELECT Top 3 ShipCountry, AVG(Freight) As AverageFreight
 FROM Orders
 GROUP BY ShipCountry
 ORDER BY AverageFreight desc;
-
